7b4048db2ba5c91f64738756697421f4e6beeb72
Edoardo Manfredi <s316714@studenti.polito.it>
2023-08-28 16:18:43 +0200

    wip: improving build script, adding testbench and simulation command

--- dlx/scripts/build.sh
+++ dlx/scripts/build.sh
@@ -3,6 +3,7 @@
 SIM_DIR='./sim'
 SRC_DIR='./src'
 SIM_FILE_LIST='./components'
+TESTBENCH_FILE='./testbenches/DLX_tb.vhd'
 
 echo "[INFO] Starting Compilation."
 echo "[INFO] Moving into simulation directory"
@@ -19,6 +20,7 @@ else
 fi
 
 cd "$SIM_DIR"
+echo "$TESTBENCH_FILE" >> "$SIM_FILE_LIST"
 echo "[INFO] (SETMENTOR)"
 INSTALL_DIR=/eda/mentor/2020-21/RHELx86/QUESTA-CORE-PRIME_2020.4/questasim/linux_x86_64
 export PATH=$INSTALL_DIR:$PATH
@@ -31,6 +33,11 @@ vlib work
 
 # VHDL file list
 vcom -F "$SIM_FILE_LIST"
+if [ $? -ne 0 ]; then
+    echo "There are some compilation errors"
+    exit 1
+fi
 
 # Simulation
+vsim -t 10ps work.DLX_tb -voptargs=+acc
 # Synthesis
