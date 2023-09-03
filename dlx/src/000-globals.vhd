9a4c717c4108c0a1bf7ba7e322737d91d0cc1afb
Lorenzo Ruotolo <s313207@studenti.polito.it>
2023-09-03 20:00:36 +0200

    fix(globals): ORI1 changed to ORI

--- dlx/src/000-globals.vhd
+++ dlx/src/000-globals.vhd
@@ -117,7 +117,7 @@ package myTypes is
     constant ITYPE_ADDI : opcode_t := "000001";  -- ADDI  RS1,RD,INP1
     constant ITYPE_SUBI : opcode_t := "000010";  -- SUBI  RA,RB,INP1
     constant ITYPE_ANDI : opcode_t := "000011";  -- ANDI  RA,RB,INP1
-    constant ITYPE_ORI1 : opcode_t := "000100";  -- ORI  RA,RB,INP1
+    constant ITYPE_ORI  : opcode_t := "000100";  -- ORI  RA,RB,INP1
     constant ITYPE_XORI : opcode_t := "000101";  -- XORI  RA,RB,INP2
     constant ITYPE_SGEI : opcode_t := "000110";  -- SGEI RA,RB,INP2
     constant ITYPE_SLEI : opcode_t := "000111";  -- SLEI RA,RB,INP2
