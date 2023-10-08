6684d5f68c75acecc7d88f5ee3513dc0decb8f38
Edoardo Manfredi <s316714@studenti.polito.it>
2023-10-08 21:29:16 +0200

    fix(fwd): add controls to avoid fwd of ITYPE instructions

--- dlx/src/forwarding_unit.vhd
+++ dlx/src/forwarding_unit.vhd
@@ -37,27 +37,31 @@ begin
 
     -- detect hazards in execute stage
     -- forwarding from the exe/mem stage
-    if (cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE = '1') and (unsigned(dp_to_fu.RD_EX) /= 0) then
+    if (cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE = '1')
+      and (unsigned(dp_to_fu.RD_EX) /= 0) then
       if (dp_to_fu.RD_EX = dp_to_fu.RS_ID) then
         MUX_A_SEL <= "10";
       end if;
 
-      if ((dp_to_fu.RD_EX = dp_to_fu.RT_ID) and (cu_to_fu.DRAM_ENABLE = '0')) then
+      if ((dp_to_fu.RD_EX = dp_to_fu.RT_ID) and (cu_to_fu.DRAM_ENABLE = '0') and (cu_to_fu.MUX_A_CU = '1' and cu_to_fu.MUX_B_CU = '0')) then
         MUX_B_SEL <= "10";
       end if;
     end if;
 
     -- detect hazards in memory stage
     -- forwarding from the mem/wb stage
-    if (cu_to_fu.RF_WR_MEM = '1' or cu_to_fu.DRAM_ENABLE = '1') and (unsigned(dp_to_fu.RD_MEM) /= 0) and (cu_to_fu.IS_JUMP_EX = '0') then
+    if (cu_to_fu.RF_WR_MEM = '1' or cu_to_fu.DRAM_ENABLE = '1')
+      and (unsigned(dp_to_fu.RD_MEM) /= 0)
+      and (cu_to_fu.IS_JUMP_EX = '0') then
       if ((dp_to_fu.RD_MEM = dp_to_fu.RS_ID) and
           not((cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE = '1')
               and (unsigned(dp_to_fu.RD_EX) /= 0)
               and (dp_to_fu.RD_EX = dp_to_fu.RS_ID))) then
         MUX_A_SEL <= "11";
       end if;
-      if (((dp_to_fu.RD_MEM = dp_to_fu.RT_ID) and (cu_to_fu.DRAM_ENABLE = '0'))and
-          not((cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE = '1')
+      if (((dp_to_fu.RD_MEM = dp_to_fu.RT_ID) and (cu_to_fu.DRAM_ENABLE = '0'))
+          and (cu_to_fu.MUX_A_CU = '1' and cu_to_fu.MUX_B_CU = '0')
+          and not((cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE = '1')
               and (unsigned(dp_to_fu.RD_EX) /= 0)
               and (dp_to_fu.RD_EX = dp_to_fu.RT_ID))) then
         MUX_B_SEL <= "11";
