19239971c4d2ba8639a8bfdbc4f0615d423577ba
Lorenzo Ruotolo <s313207@studenti.polito.it>
2023-10-16 10:23:18 +0200

    feat(syn): added colors to scripts

--- dlx/scripts/syn.sh
+++ dlx/scripts/syn.sh
@@ -6,36 +6,38 @@ SYN_RES='./res/syn'
 SYN_TCL='./scripts/syn.tcl'
 CLK_PERIOD_DEF=1
 
-echo "[INFO] Synthesis Setup..."
+GREEN="\e[32m"  # Green text color
+RESET="\e[0m"   # Reset text color
+
+echo -e "${GREEN}[INFO] Synthesis Setup...${RESET}"
 
 if [ $# -lt 1 ]; then
-    echo "[INFO] No clock period provided, using default $CLK_PERIOD_DEF ns period."
+    echo -e "${GREEN}[INFO] No clock period provided, using default $CLK_PERIOD_DEF ns period.${RESET}"
     CLK_PERIOD=$CLK_PERIOD_DEF
 else
     # Save the first argument in CLK_PERIOD
     CLK_PERIOD="$1"
 fi
 
-
 if [ ! -d "$SYN_DIR" ]; then
-    echo "[INFO] Folder $SYN_DIR does not exist yet, creating it now."
+    echo -e "${GREEN}[INFO] Folder $SYN_DIR does not exist yet, creating it now.${RESET}"
     mkdir -p "$SYN_DIR"
 fi
 mkdir -p "$SYN_DIR/work"
 mkdir -p "$SYN_DIR/src"
 mkdir -p "$SYN_DIR/src/datapath"
 
-echo "[INFO] Copying sources from $SRC_DIR"
+echo -e "${GREEN}[INFO] Copying sources from $SRC_DIR${RESET}"
 find "$SRC_DIR" -maxdepth 1 -type f -name "*.vhd" -exec cp --update {} "$SYN_DIR/src/" \;
 rsync -avm --include='*.vhd' --include='*/' --exclude='*' "$SRC_DIR/datapath/" "$SYN_DIR/src/datapath/"
 
-echo "[INFO] Copying other resources from $SYN_RES"
+echo -e "${GREEN}[INFO] Copying other resources from $SYN_RES${RESET}"
 cp -ra --update $SYN_RES/* $SYN_DIR/
 
-echo "[INFO] Setting up Design Vision"
+echo -e "${GREEN}[INFO] Setting up Design Vision${RESET}"
 source /eda/scripts/init_design_vision
 
-echo "[INFO] Setup done, launching Design Vision..."
+echo -e "${GREEN}[INFO] Setup done, launching Design Vision...${RESET}"
 cd $SYN_DIR
 dc_shell-t -no_gui << EOF
     set CLK_PERIOD $CLK_PERIOD
--- dlx/scripts/syn.tcl
+++ dlx/scripts/syn.tcl
@@ -1,9 +1,23 @@
 #!/usr/bin/env tclsh
 
-puts "(INFO) Starting Synthesis."
+# colors
+proc color {foreground text} {
+    return [exec tput setaf $foreground]$text[exec tput sgr0]
+}
+
+proc print_info {message} {
+	puts [ color 2 "\[INFO\] $message" ]
+}
+
+proc print_error {message} {
+	puts [ color 1 "\[ERROR\] $message" ]
+}
+
+puts "\n"
+print_info "Starting Synthesis."
 
 # clock
-puts "(INFO) Clock period: $CLK_PERIOD ns"
+print_info "Clock period: $CLK_PERIOD ns"
 set target_clk_ns $CLK_PERIOD
 
 # constants
@@ -14,7 +28,7 @@ set fcomp [open $fcomp_name r]
 set fanalyze [read $fcomp]
 
 # components analysis
-puts "(INFO) Starting Analysis."
+print_info "Starting Analysis."
 set errors 0
 foreach component [split [string trim "$fanalyze" "\n"] "\n"] {
 	if {[analyze "$component" -library WORK -format VHDL] == 0} {
@@ -22,42 +36,42 @@ foreach component [split [string trim "$fanalyze" "\n"] "\n"] {
 	}
 }
 if { $errors } {
-	puts "(ERROR) Errors during Analysis"
+	print_error "Errors during Analysis"
 	exit
 }
-puts "(INFO) Done: Analysis"
+print_info "Done: Analysis"
 
 # elaboration and synthesis settings
-puts "(INFO) Starting Elaboration."
+print_info "Starting Elaboration."
 elaborate "${design_entity}" -update -architecture "${design_arch}" -library WORK
 set_wire_load_model -name 5K_hvratio_1_4
 create_clock -name "CLK" -period $target_clk_ns CLK
 set_fix_hold "CLK"
 set_max_delay $target_clk_ns -from [all_inputs] -to [all_outputs]
-puts "(INFO) Done: Elaboration"
+print_info "Done: Elaboration"
 
 # design compile
-puts "(INFO) Starting Compile."
+print_info "Starting Compile."
 compile -map_effort high
-puts "(INFO) Done: Compile"
+print_info "Done: Compile"
 
 # generate netlist
-puts "(INFO) Generating Netlist."
+print_info "Generating Netlist."
 exec mkdir -p netlists
 write -format verilog -hierarchy -output "./netlists/${design_entity}_${target_clk_ns}ns_postsyn.v"
 exec mkdir -p design_compiler_sdc
 write_sdc "./design_compiler_sdc/${design_entity}_${target_clk_ns}ns_postsyn.sdc"
-puts "(INFO) Done: Netlist"
+print_info "Done: Netlist"
 
 # generate reports
-puts "(INFO) Generating Reports."
+print_info "Generating Reports."
 exec mkdir -p reports
 report_timing > "./reports/${design_entity}_${target_clk_ns}ns_timing.rpt"
 report_area   > "./reports/${design_entity}_${target_clk_ns}ns_area.rpt"
 report_power  > "./reports/${design_entity}_${target_clk_ns}ns_power.rpt"
-puts "(INFO) Done: Reports"
+print_info "Done: Reports"
 
 # final cleanup
-puts "(INFO) Done: Synthesis. Cleaning up..."
+print_info "Done: Synthesis. Cleaning up..."
 exec rm -rf work
 exit
