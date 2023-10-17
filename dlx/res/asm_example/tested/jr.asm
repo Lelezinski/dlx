4ab3d331c820e7cb9ee60d3f3c63f2dd1d6c76aa
Lorenzo Ruotolo <s313207@studenti.polito.it>
2023-10-17 16:32:58 +0200

    feat(test): added more asm tests

--- dlx/res/asm_example/tested/jr.asm
+++ dlx/res/asm_example/tested/jr.asm
@@ -1,11 +1,16 @@
-seq r25, r25, r25 ; r25 <= 1
-sne r20, r25, r20 ; just to try 
+    seq     r25, r25, r25 #  r25 <= 1
+    sne     r20, r25, r20 #  just to try
+    addui   r3, r0, 100
 myloop:
-add r1, r1, r25     ; r1 <= 1, 17 , 145
-add r1, r25, r1     ; r1 <= 2, 18 , 146
-add r1, r1, r1      ; r1 <= 4, 36 , 292
-add r1, r1, r1      ; r1 <= 8, 72 , 584
-add r1, r1, r1      ; r1 <= 16,144, 1168
-xor r20, r20, r25   ; toggle lsb of r20.
-addi r7, r0, myloop ;move label into r7
-jr r7        	    ;jump
+    subui   r3, r3, 50
+    subui   r3, r3, 1
+
+    add     r1, r1, r25 #  r1 <= 1, 17 , 145
+    add     r1, r25, r1 #  r1 <= 2, 18 , 146
+    add     r1, r1, r1 #  r1 <= 4, 36 , 292
+    add     r1, r1, r1 #  r1 <= 8, 72 , 584
+    add     r1, r1, r1 #  r1 <= 16,144, 1168
+    xor     r20, r20, r25 #  toggle lsb of r20.
+    addi    r6, r0, 4 # SKIP FIRST SUBUI
+    addi    r7, r6, myloop # move label into r7
+    jr      r7      # jump
