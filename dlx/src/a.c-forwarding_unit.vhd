465057ef0b983f08954da49eed8992d70175835e
Edoardo Manfredi <s316714@studenti.polito.it>
2023-10-19 10:02:33 +0200

    feat(fwd): comments everywhere

--- dlx/src/a.c-forwarding_unit.vhd
+++ dlx/src/a.c-forwarding_unit.vhd
@@ -13,10 +13,6 @@ entity forwarding_unit is
 
     -- signals coming from the datapth
     dp_to_fu : in dp_to_fu_t;
-    -- RD_EX     : in data_t;
-    -- RD_MEM    : in data_t;
-    -- RS_ID     : in data_t;
-    -- RT_ID     : in data_t;
 
     MUX_FWD_MEM_LMD_SEL : out std_logic;
     MUX_FWD_EX_LMD_SEL : out std_logic;
@@ -37,70 +33,71 @@ begin
     MUX_FWD_EX_LMD_SEL <= '0';
     MUX_FWD_BZ_SEL <= "00";
 
-    -- detect hazards in execute stage
-    -- forwarding from the exe/mem stage
-    if (cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE_MEM = '1')
-      and (cu_to_fu.MUX_COND_SEL /= "01" )
-      and (cu_to_fu.MUX_COND_SEL /= "10" )
+    -- detect hazards in execute stage. Forward them from memory stage to execute stage
+    if (cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE_MEM = '1') -- forward only instructions that write the RF or lw/sw
+      and (cu_to_fu.MUX_COND_SEL /= "01" ) -- do not forward if the instruction is a bnez
+      and (cu_to_fu.MUX_COND_SEL /= "10" ) -- do not forward if the instruction is a bez
       and (unsigned(dp_to_fu.RD_EX) /= 0) then
-      if (dp_to_fu.RD_EX = dp_to_fu.RS_ID) then
+
+      if (dp_to_fu.RD_EX = dp_to_fu.RS_ID) then -- forward the first operand of the alu
         MUX_A_SEL <= "10";
       end if;
 
-      if ((dp_to_fu.RD_EX = dp_to_fu.RT_ID) and (cu_to_fu.DRAM_ENABLE_MEM = '0') and (cu_to_fu.MUX_A_CU = '1' and cu_to_fu.MUX_B_CU = '0')) then
+      if ((dp_to_fu.RD_EX = dp_to_fu.RT_ID) -- forward the second operand of the alu
+          and (cu_to_fu.DRAM_ENABLE_MEM = '0') -- do not forward the second operand if the instruction is a lw/sw, because they always use the immediate field
+          and (cu_to_fu.MUX_A_CU = '1' and cu_to_fu.MUX_B_CU = '0')) then -- forward the second operad only if the instruction should use the data coming from B
         MUX_B_SEL <= "10";
       end if;
     end if;
 
-    -- detect hazards in memory stage
-    -- forwarding from the mem/wb stage
-    if (cu_to_fu.RF_WR_MEM = '1' or cu_to_fu.DRAM_ENABLE_MEM = '1')
-      and (cu_to_fu.MUX_COND_SEL /= "01" )
-      and (cu_to_fu.MUX_COND_SEL /= "10" )
+    -- detect hazards in execute stage. Forward them from memory stage to execute stage
+    if (cu_to_fu.RF_WR_MEM = '1' or cu_to_fu.DRAM_ENABLE_MEM = '1') -- forward only instructions that write the RF or lw/sw
+      and (cu_to_fu.MUX_COND_SEL /= "01" ) -- do not forward if the instruction is a bnez
+      and (cu_to_fu.MUX_COND_SEL /= "10" ) -- do not forward if the instruction is a bez
       and (unsigned(dp_to_fu.RD_MEM) /= 0)
-      and (cu_to_fu.IS_JUMP_EX = '0') then
-      if ((dp_to_fu.RD_MEM = dp_to_fu.RS_ID) and
+      and (cu_to_fu.IS_JUMP_EX = '0') then -- avoid detecting false forwarding paths
+      if ((dp_to_fu.RD_MEM = dp_to_fu.RS_ID) and -- forward the first operad of the ALU
           not((cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE_MEM = '1')
               and (unsigned(dp_to_fu.RD_EX) /= 0)
-              and (dp_to_fu.RD_EX = dp_to_fu.RS_ID))) then
+              and (dp_to_fu.RD_EX = dp_to_fu.RS_ID))) then -- avoid detecting false hazards
         MUX_A_SEL <= "11";
       end if;
-      if (((dp_to_fu.RD_MEM = dp_to_fu.RT_ID) and (cu_to_fu.DRAM_ENABLE_MEM = '0'))
-          and (cu_to_fu.MUX_A_CU = '1' and cu_to_fu.MUX_B_CU = '0')
+      if (((dp_to_fu.RD_MEM = dp_to_fu.RT_ID) -- forward the second operand of the alu
+          and (cu_to_fu.DRAM_ENABLE_MEM = '0')) -- do not forward the second operand if the instruction is a lw/sw, because they always use the immediate field
+          and (cu_to_fu.MUX_A_CU = '1' and cu_to_fu.MUX_B_CU = '0') -- forward the second operad only if the instruction should use the data coming from B
           and not((cu_to_fu.RF_WR_EX = '1' or cu_to_fu.DRAM_ENABLE_MEM = '1')
               and (unsigned(dp_to_fu.RD_EX) /= 0)
-              and (dp_to_fu.RD_EX = dp_to_fu.RT_ID))) then
+              and (dp_to_fu.RD_EX = dp_to_fu.RT_ID))) then -- avoid detecting false hazards
         MUX_B_SEL <= "11";
       end if;
     end if;
 
-    if (cu_to_fu.MUX_COND_SEL = "01" or cu_to_fu.MUX_COND_SEL = "10")
-      and (unsigned(dp_to_fu.RD_EX) /= 0) and (dp_to_fu.RD_EX = dp_to_fu.RS_ID) then
+    if (cu_to_fu.MUX_COND_SEL = "01" or cu_to_fu.MUX_COND_SEL = "10") -- if the istruction is a bne or a beqz
+      and (unsigned(dp_to_fu.RD_EX) /= 0) and (dp_to_fu.RD_EX = dp_to_fu.RS_ID) then -- forward data to check the branch result from memory
       MUX_FWD_BZ_SEL <= "10";
     elsif (cu_to_fu.MUX_COND_SEL = "01" or cu_to_fu.MUX_COND_SEL = "10") and
       (unsigned(dp_to_fu.RD_EX) /= 0) and
-      (dp_to_fu.RD_MEM = dp_to_fu.RS_ID) then
+      (dp_to_fu.RD_MEM = dp_to_fu.RS_ID) then -- forward data to check the branch result from write back stage
         MUX_FWD_BZ_SEL <= "11";
     end if;
 
     -- forward loaded data if storing it is needed
-    if ((cu_to_fu.DRAM_READNOTWRITE = '0') and
+    if ((cu_to_fu.DRAM_READNOTWRITE = '0') and -- it exists but it does not work
       (unsigned(dp_to_fu.RD_MEM) /= 0) and
       (dp_to_fu.RD_EX = dp_to_fu.RD_MEM)) then
       MUX_FWD_MEM_LMD_SEL <= '1';
     end if;
 
     -- forward rd or rt (destination register)
-    -- from prev instruction when sw or lw are in mem
-    if ((cu_to_fu.DRAM_ENABLE_MEM = '1') and
-        (unsigned(dp_to_fu.RD_MEM) /= 0) and
-        (dp_to_fu.RS_ID = dp_to_fu.RD_MEM)) then
+    if ((cu_to_fu.DRAM_ENABLE_MEM = '1') and -- if the instruction is a lw or sw
+        (unsigned(dp_to_fu.RD_MEM) /= 0) and -- if destination register is not 0
+        (dp_to_fu.RS_ID = dp_to_fu.RD_MEM)) then -- if the data to be stored is in the memory stage
       MUX_FWD_EX_LMD_SEL  <= '1';
     end if;
 
-    if ((cu_to_fu.DRAM_ENABLE_EX = '1') and
-        (unsigned(dp_to_fu.RD_MEM) /= 0) and
-        (dp_to_fu.RD_ID = dp_to_fu.RD_MEM)) then
+    if ((cu_to_fu.DRAM_ENABLE_EX = '1') and -- if the instruction is a lw or sw
+        (unsigned(dp_to_fu.RD_MEM) /= 0) and -- if destination register is not 0
+        (dp_to_fu.RD_ID = dp_to_fu.RD_MEM)) then -- if the data to be stored is in the memory stage
       MUX_FWD_EX_LMD_SEL  <= '1';
     end if;
 
